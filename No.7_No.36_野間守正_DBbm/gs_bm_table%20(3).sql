c4486623334fe0a3316acce7697289cc8922845c
morinoma <63494467+morinoma@users.noreply.github.com>
2020-06-11 23:39:56 +0900

    No.7_No.36_野間守正_DBbm

---
+++ No.7_No.36_野間守正_DBbm/gs_bm_table%20(3).sql
@@ -0,0 +1,61 @@
+-- phpMyAdmin SQL Dump
+-- version 4.7.0
+-- https://www.phpmyadmin.net/
+--
+-- Host: localhost:3306
+-- Generation Time: 
+-- サーバのバージョン： 5.6.34-log
+-- PHP Version: 7.1.5
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `gs_kadai`
+--
+
+-- --------------------------------------------------------
+
+--
+-- テーブルの構造 `gs_bm_table`
+--
+
+CREATE TABLE `gs_bm_table` (
+  `ユニーク値` int(12) NOT NULL,
+  `書籍名` varchar(64) NOT NULL,
+  `書籍URL` text NOT NULL,
+  `書籍コメント` text NOT NULL,
+  `登録日時` datetime NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `gs_bm_table`
+--
+ALTER TABLE `gs_bm_table`
+  ADD PRIMARY KEY (`ユニーク値`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `gs_bm_table`
+--
+ALTER TABLE `gs_bm_table`
+  MODIFY `ユニーク値` int(12) NOT NULL AUTO_INCREMENT;COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
