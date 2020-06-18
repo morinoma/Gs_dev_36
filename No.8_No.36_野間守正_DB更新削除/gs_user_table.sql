c2fe21658a08d70e69ead3250b6aa2bc30c83a3f
morinoma <63494467+morinoma@users.noreply.github.com>
2020-06-18 23:43:15 +0900

    sql

---
+++ No.8_No.36_野間守正_DB更新削除/gs_user_table.sql
@@ -0,0 +1,43 @@
+-- phpMyAdmin SQL Dump
+-- version 4.7.0
+-- https://www.phpmyadmin.net/
+--
+-- Host: localhost:3306
+-- Generation Time: Jun 18, 2020 at 02:41 PM
+-- Server version: 5.6.34-log
+-- PHP Version: 7.1.5
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `gs_db`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `gs_user_table`
+--
+
+CREATE TABLE `gs_user_table` (
+  `id` int(12) NOT NULL,
+  `name` varchar(64) NOT NULL,
+  `lid` varchar(128) NOT NULL,
+  `lpw` varchar(64) NOT NULL,
+  `kanri_flg` int(1) NOT NULL,
+  `life_flg` int(1) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
